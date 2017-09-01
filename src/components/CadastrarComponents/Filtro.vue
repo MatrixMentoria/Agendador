4692f4be3c930f71365dc3248e73d81ac65fc52b
Tovirs <juniorv84@gmail.com>
2017-09-01 01:51:39 -0300

    Renderizando elementos dinamicamente

--- src/components/CadastrarComponents/Filtro.vue
+++ src/components/CadastrarComponents/Filtro.vue
@@ -4,19 +4,15 @@
         <div class="container">
             <div class="row">
                 <div class="col s6">
-                    <select class="browser-default" v-model="selecao.disciplina" @change="exportaDados">
+                    <select class="browser-default"  v-model="unidades">
                         <option value="" disabled>Disciplina:</option>
-                        <option value="1234AB">Arquitetura de Computadores</option>
-                        <option value="1234CD">Banco de Dados I</option>
-                        <option value="1234EF">Matemática Discreta</option>
+                        <option v-for="disciplina in disciplinas" :key="disciplina.horarios" v-bind:value="disciplina.unidades">{{ disciplina.descricao }}</option>
                     </select>
                 </div>
                 <div class="col s6">
-                    <select class="browser-default" v-model="selecao.unidade" @change="exportaDados">
+                    <select class="browser-default" v-model="horarios" @change="exportaDados">
                         <option value="" disabled>Unidade:</option>
-                        <option value="1">Unidade1</option>
-                        <option value="2">Unidade2</option>
-                        <option value="3">Unidade3</option>
+                        <option v-for="unidade in unidades" :key="unidade.codigo" v-bind:value="unidade.horarios">{{ unidade.descrição }}</option>
                     </select>
                 </div>
             </div>
@@ -26,24 +22,24 @@
 </template>
 
 <script>
-import { Dados } from './Dados.js'
+    import { Dados } from './Dados.js'
+    import disciplinasJSON from '../../../dados_json/disciplinascompleto.json'
 
 export default {
     data() {
         return {
-            selecao: {
-                disciplina: '',
-                unidade: '',
-            },
+            disciplinas: '',
+            unidades: '',
+            horarios: '',
         };
+    },
+    mounted: function() {
+        this.disciplinas = disciplinasJSON.disciplinas;
     },
     methods: {
         exportaDados() {
-            Dados.$emit('selecao',this.selecao);
+            Dados.$emit('filtro',this.horarios);
         },
     },
-}
+};
 </script>
-
-<style>
-</style>
