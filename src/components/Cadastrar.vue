e2ff69712e3f2225de4579387a26d4eb9eca9520
LuanPeixotop <luanpeixotopereira@gmail.com>
2017-09-03 13:09:52 -0300

    Corrigido erro de segurança que permitia usuário acessar página de cadastramento

--- src/components/Cadastrar.vue
+++ src/components/Cadastrar.vue
@@ -13,6 +13,7 @@ import {firebaseauth} from '../FirebaseAuth'
 import Navbar from './CadastrarComponents/Navbar';
 import Filtro from './CadastrarComponents/Filtro';
 import Tabela from './CadastrarComponents/Tabela';
+import sMDAJSON from '../sMDA.json'
 
 
 export default {
@@ -23,14 +24,31 @@ export default {
       Tabela,
       //NovoCadastro,
     },
+    data() {
+      return {
+        smdajson: sMDAJSON,
+      }
+    },
       mounted: function() {
         $(".button-collapse").sideNav();
     },
-    beforeCreate: function() {
+    created: function() {
+      var smdaLenght = Object.keys(this.smdajson.smda).length;
+      var smdaFirebase = this.smdajson;
+      var smda = false;
         firebaseauth.onAuthStateChanged(function(user) {
             if (!user) {
             alert("deslogado, entre para poder acessar essa página");
             window.location.href = "/";
+            }
+            for (var i = 0; i < smdaLenght; i++) {
+              if (user.uid == smdaFirebase.smda[i].smdaCode){
+                smda = true;
+              }
+            }
+            if (smda === false) {
+              alert("Você não tem permissão para acessar essa página");
+              window.location.href = "/";
             }
         });
     }
