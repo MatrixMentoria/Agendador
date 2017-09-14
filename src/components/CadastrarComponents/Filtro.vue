777a6e752d1cef30bfe90ed296abb8f01fd067e9
yasutot <yasuto@ymail.com>
2017-09-14 02:34:32 -0300

    edição dos status dos cadastros agora no firebase e não no local storage. checkbox alterado para mostrar todos os cadastros quando selecionado ou somente os disponíveis quando não selecionado

--- src/components/CadastrarComponents/Filtro.vue
+++ src/components/CadastrarComponents/Filtro.vue
@@ -1,20 +1,20 @@
 <template>
     <div id="filtro">
         <br>
-        <div class="container">
             <div class="row">
-                <div class="col s12">
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <i class="material-icons prefix">school</i>
-                            <input type="text" id="autocomplete-input" class="autocomplete" v-model='disciplinaSelecionada'>
-                            <label for="autocomplete-input">Disciplina</label>
-                        </div>
-                    </div>
+                <div class="container">
+                <div class="input-field col s8">
+                    <i class="material-icons prefix">school</i>
+                    <input type="text" id="autocomplete-input" class="autocomplete" v-model='disciplinaSelecionada'>
+                    <label for="autocomplete-input">Filtro de Disciplinas</label>
+                </div>
+                <div class="input-field col s4">
+                    <input type="checkbox" id="exibicao" v-model="status" @change="emitirCheckbox()"/>
+                    <label for="exibicao" >Exibir Indisponíveis</label>
+                </div>
                 </div>
             </div>
-        </div>
-        <br>
+
     </div>
 </template>
 
@@ -25,8 +25,12 @@
     const disciplinasRef = firebaseDatabase.ref('disciplina');
 
 export default {
+    firebase: {
+        disciplinas: disciplinasRef
+    },
     data() {
         return {
+            status: '',
             disciplinas: [],
             disciplinaSelecionada: '',
         };
@@ -37,25 +41,28 @@ export default {
         snapshot.forEach((item) => {               
           this.disciplinas = item.val();
         });
-        this.autoComplete();          
+        this.autoComplete();
       });
     },
     methods: {
         autoComplete: function() {
-        var listaParaAutoComplete = {};
-        for ( var i = 0 ; i < this.disciplinas.length ; i++ ) {
-            listaParaAutoComplete[this.disciplinas[i].descricao] = null
-        }
-        $(() => {
-            $('input.autocomplete').autocomplete({
-                data: listaParaAutoComplete,
-                limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
-                onAutocomplete: function(val) {
-                     Dados.$emit('filtro',val);
-                },
-                minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+            var listaParaAutoComplete = {};
+            for ( var i = 0 ; i < this.disciplinas.length ; i++ ) {
+                listaParaAutoComplete[this.disciplinas[i].descricao] = null
+            }
+            $(() => {
+                $('input.autocomplete').autocomplete({
+                    data: listaParaAutoComplete,
+                    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+                    onAutocomplete: function(val) {
+                        Dados.$emit('filtro',val);
+                    },
+                    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+                });
             });
-        });
+        },
+        emitirCheckbox: function(status) {
+            Dados.$emit('status', this.status);
         }
     }
 };
