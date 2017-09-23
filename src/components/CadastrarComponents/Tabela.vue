0bc89c56dd7a84505add1876b9c14f3dd66f69c5
Tovirs <juniorv84@gmail.com>
2017-09-23 09:55:50 -0300

    retirando paginação

--- src/components/CadastrarComponents/Tabela.vue
+++ src/components/CadastrarComponents/Tabela.vue
@@ -35,21 +35,7 @@
                 </tr>
             </tbody>
         </table>
-
-        <div class="row center">
-            <ul class="pagination">
-                <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-                <li class="active"><a href="#!">1</a></li>
-                <li class="waves-effect"><a href="#!">2</a></li>
-                <li class="waves-effect"><a href="#!">3</a></li>
-                <li class="waves-effect"><a href="#!">4</a></li>
-                <li class="waves-effect"><a href="#!">5</a></li>
-                <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-            </ul>
-        </div>
-
         <modal-edicao :horario="horarioEditado" :disciplinas="listaDeDisciplinas"></modal-edicao>
-
     </div>
 </template>
 
